afde0fe6937822de60d2452b0b0c87527070db7b
ParthGurval <sohamgurval2001@gmail.com>
2023-09-29 22:08:47 +0530

    Practice of Code
    Practice of Code

---
+++ my-react-demo/src/component/ArrowFutn.js
@@ -0,0 +1,25 @@
+const ArrowFutn = () => {
+
+    return(
+
+        <div>
+            <h1 
+                style={{
+                            color:"red", 
+                            fontSize: "28px"
+                        }}>
+                This is Head using Arrow Function
+            </h1>
+
+            <h2 
+                style={{
+                            color:"green", 
+                            fontSize: "24px"
+                        }}>This is Heading Two
+            </h2>
+        </div>
+    )
+        
+}
+
+export default ArrowFutn;
---
+++ my-react-demo/src/component/Header.js
@@ -0,0 +1,15 @@
+function Header(){
+
+    return(
+
+        // <h1>
+        //     This is Header Of React Js File.
+        // </h1>
+
+        <div>
+            <h1>This is The Header of React...!!!</h1>
+        </div>
+    )
+}
+
+export default Header
